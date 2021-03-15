098be2679f24026d2fca232bcd4865fbb9a48318
qitas <qitas@qitas.cn>
2021-03-15 11:30:21 +0800

    init

---
+++ lgt8fx8p/pins_arduino.h
@@ -0,0 +1,16 @@
+#ifndef __LGT8FX8P__
+#define	__LGT8FX8P__
+
+#ifndef __LGT8F__
+#define	__LGT8F__
+#endif
+
+#include "lgtx8p.h"
+
+static const uint8_t DAC0 = 4;
+
+#include "../standard/pins_arduino.h"
+#undef NUM_ANALOG_INPUTS
+#define NUM_ANALOG_INPUTS           8
+
+#endif
