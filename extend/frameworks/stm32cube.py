c67e050d9dbcb102f56d23ea2d94d8f728f3e253
qitas <qitas@qitas.cn>
2021-05-01 16:31:57 +0800

    fix

--- extend/frameworks/stm32cube.py
+++ extend/frameworks/stm32cube.py
@@ -1,27 +1,3 @@
-# Copyright 2014-present PlatformIO <contact@platformio.org>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-STM32Cube HAL
-
-STM32Cube embedded software libraries, including:
-The HAL hardware abstraction layer, enabling portability between different STM32 devices via standardized API calls
-The Low-Layer (LL) APIs, a light-weight, optimized, expert oriented set of APIs designed for both performance and runtime efficiency.
-
-http://www.st.com/en/embedded-software/stm32cube-embedded-software.html?querycriteria=productId=LN1897
-"""
-
 import glob
 import os
 import shutil
